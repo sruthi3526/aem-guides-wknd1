2d83dfb0f68c2beb02634ab503eab44e14143193
harmalpanideloitte <harmalpani@deloitte.com>
2024-02-07 14:58:12 +0530

    checking eslint linter

--- ui.frontend/src/main/webpack/site/main.js
+++ ui.frontend/src/main/webpack/site/main.js
@@ -10,3 +10,6 @@ import '../components/navigation/navigation.js';
 import '../components/form/sign-in-buttons/sign-in-buttons.js';
 import '../components/form/sign-in-form/sign-in-form.js';
 import '../components/contentfragment/js/contributor.js';
+
+
+
